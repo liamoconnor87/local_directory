a2156168a81580cb9379440716c4beb7529f08ed
liamoconnor87 <liamoconnor87@gmail.com>
2020-12-17 14:32:00 +0000

    Log In Feat added

--- app.py
+++ app.py
@@ -31,8 +31,28 @@ def index():
     return render_template("index.html", results=business)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # checks if username exists in the db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        # checks is password matches db
+        if check_password_hash(
+            existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(request.form.get("username")))
+
+        else:
+            # if password does not match
+            flash("Incorrect Username/Password")
+            return redirect(url_for("login"))
+
+    else:
+        # if username does not match
+        flash("Incorrect Username/Password")
+        return redirect(url_for("login"))
     return render_template("login.html")
 
 
