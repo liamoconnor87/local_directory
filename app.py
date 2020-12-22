c7afb8e2b176b23de4b6133c9f1256cf350b5438
liamoconnor87 <liamoconnor87@gmail.com>
2020-12-22 15:45:18 +0000

    Log out function redirects to Index page

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def logout():
     # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
-    return redirect(url_for("login"))
+    return redirect(url_for("index"))
 
 
 
