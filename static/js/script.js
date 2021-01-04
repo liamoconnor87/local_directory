54c180f007b4142608196d8a7f57ad16e2b6f302
liamoconnor87 <liamoconnor87@gmail.com>
2021-01-04 13:02:53 +0000

    Feat:Profile  Modal: Are you sure you want to delete

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    
+
+    // Animates Business Cards
+    $(".bus-results").delay(300).animate({
+        opacity: '1',
+    }, 300);
+
+    // Delete Profile and Buisness function 
+    $(".delete").click(function(){
+        $(".del-dsply").show()
+    })
+
+    $(".cancel").click(function(){
+        $(".del-dsply").hide()
+    })
+
   });
